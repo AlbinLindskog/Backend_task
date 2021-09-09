f1adfbc5ee452c25f58e1ec78b19b42162f3dba0
albinlindskog <albin@zerebra.com>
2021-09-09 21:20:52 +0200

    Document BinditClient.

--- README.rst
+++ README.rst
@@ -1,21 +1,15 @@
+Backend task
+************
 Your task is to build a reliable, fault-tolerant integration towards Bindit, a digital, financial service that takes
 your money.
 
 Your task is to implement the function ´create_account_and_task´ that creates an account and transfer to Bindit,
-stores the information in the db through the models ´Account´ and ´Transfer´, and most importantly gracefully handles
+stores the information in the db through the models ´Account´ and ´Transfer´, and most importantly; gracefully handles
 any errors and problems that could occur.
 
 To your help you have a python client that communicates with Bindit's API over HTTPS, ´BinditClient´. Note that you do
 not need to implement BinditClient, it is a stub, intended to be mocked in tests. Likewise are the actual tests stubs,
 you will need to write them based on what you think are important to test. You may need to write additional tests.
-
-To get up and running you will need to install the dependencies. Assuming you already have python3 installed::
-
-    $ pip3 install -r requirements.txt
-
-To run the test::
-
-    $ python3 manage.py test
 
 Spend as much or as little time as you would like, just take a note of how much time you did spend, as we will take
 that into consideration when assessing your solution.
--- integration/client.py
+++ integration/client.py
@@ -5,19 +5,87 @@ class BinditException(Exception):
 class BinditClient:
 
     def create_account(self, name):
+        """
+        Creates an account in Bindit.
+
+        :arg string name: Name of account to open.
+        :returns dict:
+        :raises BinditException: If the account could not be created.
+
+        :Example:
+            >>> BinditClient.create_account('gerbil')
+            {'id': 45, 'name': 'gerbil'}
+        """
         pass
 
     def update_account(self, id, name):
+        """
+        Updates an account in Bindit.
+
+        :arg id int: ID of account to update
+        :arg string name: New name of account
+        :returns dict:
+        :raises BinditException: If the account could not be updated.
+
+        :Example:
+            >>> BinditClient.update_account(45, 'mouse')
+            {'id': 45, 'name': 'mouse'}
+        """
         pass
 
     def delete_account(self, id):
+        """
+        Deletes an account in Bindit.
+
+        :arg id int: ID of account to delete.
+        :returns None:
+        :raises BinditException: If the account could not be created.
+
+        :Example:
+            >>> BinditClient.delete_account(45)
+        """
         pass
 
     def create_transfer(self, amount, account):
+        """
+        Creates a transfer in Bindit.
+
+        :arg int amount: Amount to transfer.
+        :arg int account: ID of account to transfer to.
+        :returns dict:
+        :raises BinditException: If the transfer could not be created.
+
+        :Example:
+            >>> BinditClient.create_transfer(12, 45)
+            {'id': 67, 'amount': 12, 'account': 45}
+        """
         pass
 
     def update_transfer(self, id, amount, account):
+        """
+        Updates a transfer in Bindit.
+
+        :arg id int: ID of transfer to update.
+        :arg int amount: New amount to transfer.
+        :arg int account: ID of new account to transfer to.
+        :returns dict:
+        :raises BinditException: If the transfer could not be updated.
+
+        :Example:
+            >>> BinditClient.create_transfer(67, 22, 45)
+            {'id': 67, 'amount': 22, 'account': 45}
+        """
         pass
 
-    def delete_transfer(self, amount, account):
+    def delete_transfer(self, id):
+        """
+        Deletes a transfer in Bindit.
+
+        :arg id int: ID of transfer to delete.
+        :returns None:
+        :raises BinditException: If the transfer could not be deleted.
+
+        :Example:
+            >>> BinditClient.delete_transfer(67)
+        """
         pass
